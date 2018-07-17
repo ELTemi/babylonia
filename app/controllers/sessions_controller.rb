77dd21f34887411b583a4d8706cd7fb67ce639e9
Ruth <github email address>
2018-07-17 13:45:19 -0400

    add logout action in sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,16 +7,25 @@ class SessionsController < ApplicationController
   def create
     if params[:user][:admin] == "1"
       @caregiver = Caregiver.find_by(email: params[:user][:email])
-      session[:user_id] = @caregiver.id
-      redirect_to caregiver_path(@caregiver)
+      if @caregiver.authenticate(params[:user][:password])
+        session[:user_id] = @caregiver.id
+        redirect_to caregiver_path(@caregiver)
+      else
+        render :login
+      end
     else
       @mom = Mom.find_by(email: params[:user][:email])
-      session[:user_id] = @mom.id
-      redirect_to mom_path(@mom)
+      if @mom.authenticate(params[:user][:password])
+        session[:user_id] = @mom.id
+        redirect_to mom_path(@mom)
+      else
+        render :login
+      end
     end
   end
 
   def logout
-
+    session.delete :user_id
+    redirect_to root_url
   end
 end
