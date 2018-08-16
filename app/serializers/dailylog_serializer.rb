2542236cbb98694be09cf16a6c12ccac917a462e
Ruth <github email address>
2018-08-16 12:24:05 -0400

    add instance methods for time in and out to dailylog serializer attributes

--- app/serializers/dailylog_serializer.rb
+++ app/serializers/dailylog_serializer.rb
@@ -1,5 +1,5 @@
 class DailylogSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :date_format_for_time_in, :time_in_format, :nap, :meal, :diaper, :play_time, :summary, :meds, :time_out_format
   belongs_to :baby
   belongs_to :caregiver
 end
