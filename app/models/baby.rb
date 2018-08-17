564bc0bc7622beea80acf09a8aa512b4f5598ec3
Ruth <github email address>
2018-08-17 15:16:05 -0400

    add validations for presence to baby model

--- app/models/baby.rb
+++ app/models/baby.rb
@@ -3,6 +3,7 @@ class Baby < ApplicationRecord
   belongs_to :caregiver
   has_many :dailylogs
   has_attached_file :avatar
+  validates_presence_of :name, :sex, :dob, :emergency_contact, :caregiver
   validates_uniqueness_of :name, scope: :mom_id, message: "This baby has already been added, check name"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
