1e9b0c753ea6e119017692310d841bd94ca2acbe
Ruth <github email address>
2018-08-17 15:16:39 -0400

    add attributes to baby serializer

--- app/serializers/baby_serializer.rb
+++ app/serializers/baby_serializer.rb
@@ -1,3 +1,6 @@
 class BabySerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :sex, :dob, :avatar, :allergies, :emergency_contact
+  belongs_to :mom
+  belongs_to :caregiver
+  has_many :dailylogs
 end
