05925f9bc0ba462284477f45c7ed4ad6f4a9b3f9
Ruth <github email address>
2018-08-20 13:04:13 -0400

    rewrite shownextlog function

--- app/assets/javascripts/dailylog.js
+++ app/assets/javascripts/dailylog.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   addEventListeners()
 });
 
+
 function addEventListeners() {
   $(".js-more").click(() => showMoreLogs())
   $(".js-next").click(() => showNextLog())
@@ -9,13 +10,11 @@ function addEventListeners() {
 
 
 function showMoreLogs() {
-
   var babyId = $("h4.logs-js").data("id")
   if (babyId) {
     $.get("/babies/" + babyId + "/logs", function(data) {
       const logs = data
       var logsList = ""
-
       logs.forEach(function(log) {
         logsList +=
         '<button class="js-next"' + "data-id=" + `${log["id"]}` + '>' + log["date_format_for_time_in"] + '</button>' + '<br>' +
@@ -31,8 +30,8 @@ function showMoreLogs() {
         '</li>'
       })
 
-      $('ul.baby-logs').empty();
-      $("ul.baby-logs").append(logsList)
+      $('div#babyLogs').empty();
+      $("div#babyLogs").append(logsList)
     })
   } else {
     $.get("/logs", function(data) {
@@ -64,49 +63,21 @@ function showMoreLogs() {
 }
 
 function showNextLog() {
-  var nextId = parseInt($(".js-next").attr("data-id")) + 1
-  $.get("/dailylogs/" + nextId + "/log", function(data) {
-    const log = data
-    $(".logBaby").text("Baby: " + log["baby"]["name"])
-    $(".logDay").text(log["date_format_for_time_in"])
-    $(".logTimeIn").text("Time In: " + log["time_in_format"])
-    $(".logNap").text("Napped " + log["nap"] + " times")
-    $(".logMeal").text("Ate " + log["meal"] + " times")
-    $(".logPlay").text("Played for " + log["play_time"] + " hours")
-    $(".logDiaper").text("Changed diapers " + log["diaper"] + " times")
-    $(".logComments").text("General Comments for Today: " + log["summary"])
-    $(".logTimeOut").text("Time Out: " + log["time_out_format"])
-    $(".js-next").attr("data-id", data["id"]);
-    })
-  }
-
-
-/*
-if babyID exists on the logs index show page with jquery
-else if (parseInt($(".logs a").attr("href").slice(-1))){
-  var babyIdFromLogs = parseInt($(".logs a").attr("href").slice(-1))
-  $.get("/babies/" + babyIdFromLogs + "/logs", function(data) {
-    const logs = data
-    var logsList = ""
-
-    logs.forEach(function(log) {
-      logsList +=
-      '<button class="js-next"' + "data-id=" + `${log["id"]}` + '>' + log["date_format_for_time_in"] + '</button>' + '<br>' +
-      '<li>' + '<strong>' + log["date_format_for_time_in"] + '</strong>'+ '<br>' +
-      '<strong>Time In: </strong>' +  log["time_in_format"] + '<br>' +
-      '<strong>Naps: </strong>' + log["nap"] + ' times' + '<br>' +
-      '<strong>Meal: </strong>' + log["meal"] + ' times' +  '<br>' +
-      '<strong>Diaper Changes: </strong>' + log["diaper"] + ' times' + '<br>' +
-      '<strong>Meds: </strong>' + log["meds"] + '<br>' +
-      '<strong>Playtime: </strong>' + log["play_time"] + ' times' +  '<br>' +
-      '<strong>Summary: </strong>' + log["summary"] + '<br>' +
-      '<strong>Time Out: </strong>' + log["time_out_format"] + '<br>'
-      '</li>'
-    })
-
-    $("div.logs").empty();
-    $("div.logs").append(logsList)
-  })
-
+  $.get("/logs", function(data) {
+    for (i= 0; i < data.length; i++) {
+      var nextId = data[i].id
+      $.get("/dailylogs/" + nextId + "/log", function(data) {
+        const log = data
+        $(".logBaby").text("Baby: " + log["baby"]["name"])
+        $(".logDay").text(log["date_format_for_time_in"])
+        $(".logTimeIn").text("Time In: " + log["time_in_format"])
+        $(".logNap").text("Napped " + log["nap"] + " times")
+        $(".logMeal").text("Ate " + log["meal"] + " times")
+        $(".logPlay").text("Played for " + log["play_time"] + " hours")
+        $(".logDiaper").text("Changed diapers " + log["diaper"] + " times")
+        $(".logComments").text("General Comments for Today: " + log["summary"])
+        $(".logTimeOut").text("Time Out: " + log["time_out_format"])
+      })
+    }
+  });
 }
-*/
