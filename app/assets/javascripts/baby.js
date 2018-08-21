8fc91522224374c2d023a3cb5e10978010f0da6d
Ruth <github email address>
2018-08-21 11:16:07 -0400

    debug show next baby page

--- app/assets/javascripts/baby.js
+++ app/assets/javascripts/baby.js
@@ -17,57 +17,58 @@ function addEventListenersForBaby() {
 
 
 
-  $(".js-next-baby").click(() => showNextBaby())
+  $(".js-next-baby").click(function(e) {
+    e.preventDefault();
+    showNextBaby();
+  });
 }
 
-/*$("#babyName").text(baby["name"]) + " - " + $("a#babyLogs").text("See all Logs for " + baby["name"]) + $("a#babyLogs").href("/babies/" + baby["id"] + "/dailylogs")
-*/
+
 
 function showNextBaby() {
-  $.get("/babies" + ".json", function(data) {
-    for (i= 0; i < data.length; i++) {
-      var nextId = data[i].id
-      $.get("/babies/" + nextId + "/baby",  function(data) {
-        var logs = data["dailylogs"]
-        var logsList = ""
+    var baby_id = $('#baby-id').attr('data-baby-id');
 
-          $(".babyName").text("Hi, " + data["name"])
-          $("#babyName").text("Baby Name: " + data["name"])
-          $("img").attr("src", data["avatar"])
-          $("#babyContact").text("Emergency Contact: " + data["emergency_contact"])
-          $("#babySex").text("Sex: " + data["sex"])
-          $("#babyAllergies").text("Allergies: " + data["allergies"])
-          $("#babyDOB").text("Date of Birth: " + data["dob"])
-          $("#editBaby a").attr("href", `/babies/${data["id"]}/edit`)
-          $("#editBaby a").text("Edit " + data["name"] + " Profile")
-          $("h3#babyMom").empty()
-          $("button.js-more").empty()
-          $("h4.logs-js").empty()
-          $("p.logs-js").text("Logs")
-          $(".js-next-baby").attr("data-id", data["id"]);
+    $.get("/babies/" + baby_id + "/baby",  function(data) {
+      var logs = data["dailylogs"]
+      var logsList = ""
 
-          logs.forEach(function(log) {
-            logsList +=
+      window.history.pushState(null, null, "/babies/" + data.id.toString());
 
-            '<li>' + '<strong>' + log["date_format_for_time_in"] + '</strong>'+ '<br>' +
 
-            '<strong>Time In: </strong>' +  log["time_in_format"] + '<br>' +
-            '<strong>Naps: </strong>' + log["nap"] + ' times' + '<br>' +
-            '<strong>Meal: </strong>' + log["meal"] + ' times' +  '<br>' +
-            '<strong>Diaper Changes: </strong>' + log["diaper"] + ' times' + '<br>' +
-            '<strong>Meds: </strong>' + log["meds"] + '<br>' +
-            '<strong>Playtime: </strong>' + log["play_time"] + ' times' +  '<br>' +
-            '<strong>Summary: </strong>' + log["summary"] + '<br>' +
-            '<strong>Time Out: </strong>' + log["time_out_format"] + '<br>' +
-            '</li>'
-          })
+      $(".babyName").text("Hi, " + data["name"])
+      $("#baby-id").attr("data-baby-id", data['id'])
+      $("#babyName").text("Baby Name: " + data["name"])
+      $("img").attr("src", data["avatar"])
+      $("#babyContact").text("Emergency Contact: " + data["emergency_contact"])
+      $("#babySex").text("Sex: " + data["sex"])
+      $("#babyAllergies").text("Allergies: " + data["allergies"])
+      $("#babyDOB").text("Date of Birth: " + data["dob"])
+      $("#editBaby a").attr("href", `/babies/${data["id"]}/edit`)
+      $("#editBaby a").text("Edit " + data["name"] + " Profile")
+      $("h3#babyMom").empty()
+      $("button.js-more").empty()
+      $("h4.logs-js").empty()
+      $("p.logs-js").text("Logs")
 
-          $("div#babyLogs").empty();
-          $("div#babyLogs").append(logsList)
+      logs.forEach(function(log) {
+        logsList +=
 
+        '<li>' + '<strong>' + log["date_format_for_time_in"] + '</strong>'+ '<br>' +
+
+        '<strong>Time In: </strong>' +  log["time_in_format"] + '<br>' +
+        '<strong>Naps: </strong>' + log["nap"] + ' times' + '<br>' +
+        '<strong>Meal: </strong>' + log["meal"] + ' times' +  '<br>' +
+        '<strong>Diaper Changes: </strong>' + log["diaper"] + ' times' + '<br>' +
+        '<strong>Meds: </strong>' + log["meds"] + '<br>' +
+        '<strong>Playtime: </strong>' + log["play_time"] + ' times' +  '<br>' +
+        '<strong>Summary: </strong>' + log["summary"] + '<br>' +
+        '<strong>Time Out: </strong>' + log["time_out_format"] + '<br>' +
+        '</li>'
       })
-    }
-  })
+
+      $("div#babyLogs").empty();
+      $("div#babyLogs").append(logsList);
+    })
 }
 
 
