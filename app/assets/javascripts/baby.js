21dbdb9045faf6df481a850af112af0725f7c0f3
Ruth <github email address>
2018-08-21 13:23:38 -0400

    refactor show next baby

--- app/assets/javascripts/baby.js
+++ app/assets/javascripts/baby.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
 
 function addEventListenersForBaby() {
-  $('form#new_baby.new_baby').submit(function(event) {
+  $('form#new_baby').submit(function(event) {
     event.preventDefault();
     var values = $(this).serialize();
     var posting = $.post('/babies', values);
@@ -16,7 +16,6 @@ function addEventListenersForBaby() {
   });
 
 
-
   $(".js-next-baby").click(function(e) {
     e.preventDefault();
     showNextBaby();
@@ -25,48 +24,13 @@ function addEventListenersForBaby() {
 
 
 function showNextBaby() {
-    var baby_id = $('#baby-id').attr('data-baby-id');
-
-    $.get("/babies/" + baby_id + "/baby",  function(data) {
-      var logs = data["dailylogs"]
-      var logsList = ""
-
-      window.history.pushState(null, null, "/babies/" + data.id.toString());
-
-
-      $(".babyName").text("Hi, " + data["name"])
-      $("#baby-id").attr("data-baby-id", data['id'])
-      $("#babyName").text("Baby Name: " + data["name"])
-      $("img").attr("src", data["avatar"])
-      $("#babyContact").text("Emergency Contact: " + data["emergency_contact"])
-      $("#babySex").text("Sex: " + data["sex"])
-      $("#babyAllergies").text("Allergies: " + data["allergies"])
-      $("#babyDOB").text("Date of Birth: " + data["dob"])
-      $("#editBaby a").attr("href", `/babies/${data["id"]}/edit`)
-      $("#editBaby a").text("Edit " + data["name"] + " Profile")
-      $("h3#babyMom").empty()
-      $("h4.logs-js").empty()
-      $("p.logs-js").text("Logs")
-
-      logs.forEach(function(log) {
-        logsList +=
-
-        '<li>' + '<strong>' + log["date_format_for_time_in"] + '</strong>'+ '<br>' +
-
-        '<strong>Time In: </strong>' +  log["time_in_format"] + '<br>' +
-        '<strong>Naps: </strong>' + log["nap"] + ' times' + '<br>' +
-        '<strong>Meal: </strong>' + log["meal"] + ' times' +  '<br>' +
-        '<strong>Diaper Changes: </strong>' + log["diaper"] + ' times' + '<br>' +
-        '<strong>Meds: </strong>' + log["meds"] + '<br>' +
-        '<strong>Playtime: </strong>' + log["play_time"] + ' times' +  '<br>' +
-        '<strong>Summary: </strong>' + log["summary"] + '<br>' +
-        '<strong>Time Out: </strong>' + log["time_out_format"] + '<br>' +
-        '</li>'
-      })
-
-      $("div#babyLogs").empty();
-      $("div#babyLogs").append(logsList);
-    })
+  var baby_id = $('#baby-id').attr('data-baby-id');
+  $.get("/babies/" + baby_id + "/baby",  function(data) {
+    var logs = data["dailylogs"]
+    window.history.pushState(null, null, "/babies/" + data.id.toString());
+    nextBabyShow(data)
+    logShow(logs)
+  })
 }
 
 
@@ -94,4 +58,42 @@ function clearForm() {
   $("#baby_dob").val('')
   $("#baby_sex").val('')
   $("#baby_allergies").val('')
+}
+
+function logShow(logs) {
+
+  var logsList = ""
+  logs.forEach(function(log) {
+    logsList +=
+    '<li>' + '<strong>' + log["date_format_for_time_in"] + '</strong>'+ '<br>' +
+
+    '<strong>Time In: </strong>' +  log["time_in_format"] + '<br>' +
+    '<strong>Naps: </strong>' + log["nap"] + ' times' + '<br>' +
+    '<strong>Meal: </strong>' + log["meal"] + ' times' +  '<br>' +
+    '<strong>Diaper Changes: </strong>' + log["diaper"] + ' times' + '<br>' +
+    '<strong>Meds: </strong>' + log["meds"] + '<br>' +
+    '<strong>Playtime: </strong>' + log["play_time"] + ' times' +  '<br>' +
+    '<strong>Summary: </strong>' + log["summary"] + '<br>' +
+    '<strong>Time Out: </strong>' + log["time_out_format"] + '<br>' +
+    '</li>'
+  })
+
+  $("h4.logs-js").empty()
+  $("p.logs-js").text("Logs")
+  $("div#babyLogs").empty();
+  $("div#babyLogs").append(logsList);
+}
+
+function nextBabyShow(data) {
+  $(".babyName").text("Hi, " + data["name"])
+  $("#baby-id").attr("data-baby-id", data['id'])
+  $("#babyName").text("Baby Name: " + data["name"])
+  $("img").attr("src", data["avatar"])
+  $("#babyContact").text("Emergency Contact: " + data["emergency_contact"])
+  $("#babySex").text("Sex: " + data["sex"])
+  $("#babyAllergies").text("Allergies: " + data["allergies"])
+  $("#babyDOB").text("Date of Birth: " + data["dob"])
+  $("#editBaby a").attr("href", `/babies/${data["id"]}/edit`)
+  $("#editBaby a").text("Edit " + data["name"] + " Profile")
+  $("h3#babyMom").empty()
 }
